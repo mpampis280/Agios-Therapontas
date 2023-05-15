fdb8aea6e5c9dcc5b6c0a86bdea5972f1f568d08
mpampis280 <40506303+mpampis280@users.noreply.github.com>
2023-05-15 15:12:31 +0300

    Add neos_naos and video lightbox attempt

--- js/main.js
+++ js/main.js
@@ -14,3 +14,46 @@
     }
   }
 //<!--end of lightbox palios naos toggle-->
+
+//lightbox neos photo 
+function lightbox_neos() {
+  var x = document.getElementById('lightbox_neos');
+  var y = document.getElementById('lightbox_neos_vid');
+  var op = document.getElementById('btn_neos');
+  var op2 = document.getElementById('btn_neos_vid');
+  y.style.display = 'none';
+  op2.style.opacity = 1;
+
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    op.style.opacity = 1;
+  } else {
+    x.style.display = 'block';
+    op.style.opacity = 0.3;
+  }
+}
+
+//end of lightbox neos photo
+
+//lightbox neos video
+function lightbox_neos_vid() {
+  var x = document.getElementById('lightbox_neos_vid');
+  var y = document.getElementById('lightbox_neos');
+  var op = document.getElementById('btn_neos_vid');
+  var op2 = document.getElementById('btn_neos');
+  y.style.display = 'none';
+  op2.style.opacity = 1;
+  
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    op.style.opacity = 1;
+  } else {
+    x.style.display = 'block';
+    op.style.opacity = 0.3;
+  }
+}
+
+
+
+
+//end of lightbox neos video
