d5701b48165459ae6b7398b370ae0112233a54c9
mpampis280 <40506303+mpampis280@users.noreply.github.com>
2023-05-28 16:45:04 +0300

    Added activities

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //<!--contact-->
-//<iframe class='responsive-iframe' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10953.638376042767!2d26.554947270929674!3d39.10568384260895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14ba670eaa255555%3A0xffc627be89a91314!2sGreek%20Orthodox%20Church%20of%20Saint%20Therapon!5e0!3m2!1sen!2sgr!4v1684434748420!5m2!1sen!2sgr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
 function contact_us() {
   var x = document.getElementById('contact');
   var op = document.getElementById('toggle_contact');
@@ -8,6 +8,7 @@ function contact_us() {
     op.style.color = 'black';
   } else {
     x.style.display = 'block';
+    window.scrollTo(0, 30000);
     op.style.color = 'blue';
   }
 }
@@ -85,3 +86,46 @@ function lightbox_vios() {
   }
 }
 //<!--end of lightbox vios agiou-->
+
+
+
+//lightbox activities photo 
+function lightbox_activities() {
+  var x = document.getElementById('lightbox_activities');
+  var y = document.getElementById('lightbox_activities_vid');
+  var op = document.getElementById('btn_activities');
+  var op2 = document.getElementById('btn_activities_vid');
+  y.style.display = "none";
+  op2.style.opacity = 1;
+
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    op.style.opacity = 1;
+  } 
+  else {
+    x.style.display = 'block';
+    op.style.opacity = 0.3;
+  }
+}
+
+//end of lightbox activities photo
+
+//lightbox activities video
+function lightbox_activities_vid() {
+  var x = document.getElementById('lightbox_activities_vid');
+  var y = document.getElementById('lightbox_activities');
+  var op = document.getElementById('btn_activities_vid');
+  var op2 = document.getElementById('btn_activities');
+  y.style.display = 'none';
+  op2.style.opacity = 1;
+  
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    op.style.opacity = 1;
+  } else {
+    x.style.display = 'block';
+    op.style.opacity = 0.3;
+  }
+}
+
+//end of lightbox activities video
